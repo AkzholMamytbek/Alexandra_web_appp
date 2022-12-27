047d6be1aad94a396c172fd22a5f5d7ab6e81f2e
quanyshhh <quanyshh@gmail.com>
2022-12-27 15:18:52 +0600

    updates

--- alexandra/settings.py
+++ alexandra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dxwfmt_926=6ftsc4ap_&rcy%1fl&gnr&dkkcngkpr_b6k$de#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '172.16.3.23']
 
 
 # Application definition
